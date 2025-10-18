b779fa9e675dd06b413cce238eaf0cbcff1831d6
deborahjpliegog <debyneko15@gmail.com>
2025-10-18 10:13:42 -0600

    Macros

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 """ desde donde arrancará mi proyecto es lo que indica el ('/') """
-@app.route('/')
-def home():
-    return "Hola mundo de Debi"
+@app.route('/index')
+def index():
+    titulo = "Pagina de inicio"
+    listado = ['Python', 'Flask', 'Jinja2', 'HTML', 'CSS']
+    return render_template('index.html', titulo = titulo, listado = listado)
 
-@app.route('/hola')
-def about():
-    return "Hola desde carpeta hola"
+@app.route('/calculos')
+def calculos():
+    return render_template('calculos.html')
+
+@app.route('/distancia')
+def distancia():
+    return render_template('distancia.html')
 
 """ variable tipo string llamada user """
 @app.route('/user/<string:user>')
@@ -38,6 +45,23 @@ def func1(n1, n2):
 @app.route('/default/<string:dft>')
 def func2(dft="sss"):
     return "El valor de dft es: "+dft
+
+""" ----------------------------- Hoy 18 de octubre ----------------------------- """
+@app.route("/prueba")
+def func4():
+    return ''' 
+    <html>
+        <head>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <title>Pagina Prueba</title>
+        </head>
+        <body>
+            <h1>Hola, esta es una página de prueba</h1>
+            <p>Esta página es para probar el retorno de HTML en Flask</p>
+        </body>
+    </html> 
+'''
+
 
 """ crear sistema de arranque del proyecto """
 if __name__ == '__main__':
